25c53be4313baf064f826ba2624b0db02e5ef6b4
Bojidar Ivanov <bojidarit@gmail.com>
2019-03-08 10:29:15 +0200

    Udemy - the-complete-aspnet-mvc-5-course - Exercise 1 - Customers controller with index and details implemented

---
+++ asp.net-mvc/the-complete-aspnet-mvc-5-course/src/Vidly/Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿namespace Vidly.Controllers
+{
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Web.Mvc;
+
+	public class CustomerController : Controller
+    {
+		private static IEnumerable<Models.Customer> customers = //new List<Models.Customer>();
+			new List<Models.Customer>(Enumerable.Range(1, 5).Select(i => new Models.Customer(i, $"Customer {i}")));
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+			return View(customers);
+        }
+
+		// GET: Customet/Details/{id}
+		public ActionResult Details(int id)
+		{
+			Models.Customer customer = customers.FirstOrDefault(c => c.Id == id);
+
+			if (customer != null)
+			{
+				return View(customer);
+			}
+			else
+			{
+				return HttpNotFound($"There is no customer with Id = {id}.");
+			}
+		}
+    }
+}
--- asp.net-mvc/the-complete-aspnet-mvc-5-course/src/Vidly/Vidly/Models/Movie.cs
+++ asp.net-mvc/the-complete-aspnet-mvc-5-course/src/Vidly/Vidly/Models/Movie.cs
@@ -2,6 +2,17 @@
 {
 	public class Movie
 	{
+		public Movie()
+		{
+
+		}
+
+		public Movie(int id, string name)
+		{
+			this.Id = id;
+			this.Name = name;
+		}
+
 		public int Id { get; set; }
 		public string Name { get; set; }
 	}
