ad5168d0175381b920ffb314bb60d6c22179d7d4
Bojidar Ivanov <bojidarit@gmail.com>
2019-03-16 14:56:06 +0200

    JQuery validation issue - date format validation issue with WebKit browsers fixed with override the validate date function

--- asp.net-mvc/the-complete-aspnet-mvc-5-course/src/Vidly/Vidly/Scripts/jquery.validate.date.js
+++ asp.net-mvc/the-complete-aspnet-mvc-5-course/src/Vidly/Vidly/Scripts/jquery.validate.date.js
@@ -1,4 +1,8 @@
-﻿$(function () {
+﻿// Source: https://www.itorian.com/2015/04/the-field-date-must-be-date-error-in.html
+// Issue: Basically, this issue is related with all Webkit based web browsers. 
+// Solution: The best solution for me was to override the validate date function from jquery.validate.js.
+
+$(function () {
 	$.validator.methods.date = function (value, element) {
 		if ($.browser.webkit) {
 			var d = new Date();
