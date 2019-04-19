162329717c832792cd7bf1be9bad5749e393d884
Bojidar Ivanov <bojidarit@gmail.com>
2019-04-19 10:30:41 +0300

    WPF Client to WEB API - Data window for new/edit product - OK/Cancel default buttons discarted with custom mode.

--- wpf/src/ConsumingWebApiFromWpf/WPFClientApp/Views/ManageProductWindow.xaml.cs
+++ wpf/src/ConsumingWebApiFromWpf/WPFClientApp/Views/ManageProductWindow.xaml.cs
@@ -3,7 +3,7 @@
 	public partial class ManageProductWindow
 	{
 		public ManageProductWindow()
-			: base(Catel.Windows.DataWindowMode.Custom)
+			: base(Catel.Windows.DataWindowMode.Custom)	// Custom mode for dialogs discards the default OK/Cancel buttons
 		{
 			InitializeComponent();
 		}
