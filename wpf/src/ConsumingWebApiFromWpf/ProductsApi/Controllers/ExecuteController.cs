3e08e751f7adb0e2b00ffa7339fad004c9b23ad8
Bojidar Ivanov <bojidarit@gmail.com>
2019-04-23 23:27:29 +0300

    Products Web API with new controller for executing methods with parameters (using query string).

---
+++ wpf/src/ConsumingWebApiFromWpf/ProductsApi/Controllers/ExecuteController.cs
@@ -0,0 +1,34 @@
+﻿namespace ProductsApi.Controllers
+{
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Net.Http;
+	using System.Web.Http;
+
+	public class ExecuteController : ApiController
+	{
+		// GET: /api/execute
+		// Example: http://localhost:50118/api/execute?method=MethodName&p1=1&p2=2&p3=3
+		public IHttpActionResult GetResult()
+		{
+			// TODO: Do we need the type of the parameters?!?...
+
+			Dictionary<string, List<string>> parameters = this.ActionContext.Request.GetQueryNameValuePairs()
+				.GroupBy(i => i.Key).ToDictionary(p => p.Key, p => p.ToList().Select(i => i.Value).ToList());
+
+			// When there are one or more parameter(s) with the same names => return bad request 
+			var badParams = parameters.Where(i => i.Value.Count > 1);
+			if (badParams.Any())
+			{
+				string badOnece = string.Join(",", badParams.Select(p => $"'{p.Key}'"));
+				return BadRequest($"ERROR - There are parameter(s) used more than once: [{badOnece}]");
+			}
+
+			var dictionary = parameters.ToDictionary(i => i.Key, i => i.Value.FirstOrDefault());
+
+			// TODO: Execute method with parameters that came from the query string ... 
+
+			return Ok(dictionary);
+		}
+	}
+}
