ebf64e66c650fac37d3e19219d66e1e380985834
Bojidar Ivanov <bojidarit@gmail.com>
2019-04-26 10:45:43 +0300

    Web API - Execution controller - fixed issue with the dot in the URL - web configuration added runAllManagedModulesForAllRequests. New simpler GET Action with specific route.

--- wpf/src/ConsumingWebApiFromWpf/ProductsApi/Controllers/ExecuteController.cs
+++ wpf/src/ConsumingWebApiFromWpf/ProductsApi/Controllers/ExecuteController.cs
@@ -1,5 +1,6 @@
 ﻿namespace ProductsApi.Controllers
 {
+	using System;
 	using System.Collections.Generic;
 	using System.Linq;
 	using System.Net.Http;
@@ -7,31 +8,83 @@
 
 	public class ExecuteController : ApiController
 	{
-		// GET: api/execute/
+		// GET: api/execute/{library}/{method}
 		// Example: http://localhost:50118/api/execute/oblp_users/getuser?apikey=00000&p1=1
 		[Route("api/execute/{library}/{method}")]
 		public IHttpActionResult GetResult(string library, string method)
 		{
-			// TODO: Do we need the type of the parameters?!?...
+			Dictionary<string, string> parameters = null;
 
-			Dictionary<string, List<string>> parameters = this.ActionContext.Request.GetQueryNameValuePairs()
-				.GroupBy(i => i.Key).ToDictionary(p => p.Key, p => p.ToList().Select(i => i.Value).ToList());
-
-			// When there are one or more parameter(s) with the same names => return bad request 
-			var badParams = parameters.Where(i => i.Value.Count > 1);
-			if (badParams.Any())
+			try
 			{
-				string badOnece = string.Join(",", badParams.Select(p => $"'{p.Key}'"));
-				return BadRequest($"ERROR - There are parameter(s) used more than once: [{badOnece}]");
+				parameters = QueryStringToDictionaty(this.ActionContext.Request.GetQueryNameValuePairs(), true);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
 			}
 
-			var dictionary = parameters.ToDictionary(i => i.Key, i => i.Value.FirstOrDefault());
-			dictionary.Add("library", library);
-			dictionary.Add("method", method);
-
 			// TODO: Execute method with parameters that came from the query string ... 
+			parameters.Add("library", library);
+			parameters.Add("method", method);
 
-			return Ok(dictionary);
+			return Ok(parameters);
+		}
+
+		// GET: client/{target}
+		// Example: http://localhost:50118/client/oblp_users.getuser?apikey=00000&p1=1
+		[Route("client/{target}")]
+		public IHttpActionResult GetResult(string target)
+		{
+			Dictionary<string, string> parameters = null;
+			var targetArray = target.Split('.');
+
+			if(targetArray.Length < 2)
+			{
+				return BadRequest("Wrong target. Must contains two items separated with dot symbol. " +
+					$"The current one is '{target}'");
+			}
+
+			string library = targetArray[0];
+			string method = targetArray[1];
+
+			try
+			{
+				parameters = QueryStringToDictionaty(this.ActionContext.Request.GetQueryNameValuePairs(), true);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+
+			if (parameters != null)
+			{
+				parameters.Add("library", library);
+				parameters.Add("method", method);
+				return Ok(parameters);
+			}
+
+			return BadRequest("No parameters");
+		}
+
+		private Dictionary<string, string> QueryStringToDictionaty(
+			IEnumerable<KeyValuePair<string, string>> queryKeyValuePairs, bool checkForDuplicates)
+		{
+			Dictionary<string, List<string>> parameters = queryKeyValuePairs
+				.GroupBy(i => i.Key).ToDictionary(p => p.Key, p => p.ToList().Select(i => i.Value).ToList());
+
+			if (checkForDuplicates)
+			{
+				// Check if there are one or more parameter(s) with the same names
+				var badParams = parameters.Where(i => i.Value.Count > 1);
+				if (badParams.Any())
+				{
+					string badOnece = string.Join(",", badParams.Select(p => $"'{p.Key}'"));
+					throw new System.ArgumentException($"ERROR - There are parameter(s) used more than once: [{badOnece}]");
+				}
+			}
+
+			return parameters.ToDictionary(i => i.Key, i => i.Value.FirstOrDefault());
 		}
 	}
 }
