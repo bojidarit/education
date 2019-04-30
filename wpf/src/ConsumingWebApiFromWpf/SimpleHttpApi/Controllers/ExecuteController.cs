9954997f7bcd536ed5c761a74f28f1a6822771f4
Bojidar Ivanov <bojidarit@gmail.com>
2019-04-30 18:47:08 +0300

    Simple HTTP Client - added new dummy class that represents data logic. New class used to supply data to execute controllers

--- wpf/src/ConsumingWebApiFromWpf/SimpleHttpApi/Controllers/ExecuteController.cs
+++ wpf/src/ConsumingWebApiFromWpf/SimpleHttpApi/Controllers/ExecuteController.cs
@@ -7,15 +7,10 @@
 	using System.Net.Http;
 	using System.Web.Http;
 
+	using Data = SimpleHttpApi.DataLogic;
+
 	public class ExecuteController : ApiController
 	{
-		private IEnumerable<User> _users = new List<User>()
-		{
-			new User(1, "User 1", new DateTime(2010, 1, 1), 1.23M, true),
-			new User(2, "User 2", new DateTime(2013, 12, 31), 2.34M, false),
-			new User(3, "User 3", new DateTime(2018, 1, 1), 4.56789M, true),
-		};
-
 		// GET: api/execute/{library}/{method}
 		// Example: http://localhost:50118/api/execute/oblp_users/getuser?apikey=00000&p1=1
 		[Route("api/execute/{library}/{method}")]
@@ -67,41 +62,28 @@
 
 			if (parameters != null)
 			{
-				var users = GetUsers(parameters.FirstOrDefault(i => i.Key.StartsWith("p")));
-
-				if (users.Any())
+				// TODO: needed refactoring...
+				string param = parameters.FirstOrDefault(i => i.Key.StartsWith("p")).Value;
+				if (!string.IsNullOrWhiteSpace(param))
 				{
-					return Ok(new DataListModel<User>(library, method, users));
+					var users = Data.Users.GetUser(param);
+
+					if (users.Any())
+					{
+						return Ok(new DataListModel<User>(library, method, users));
+					}
+					else
+					{
+						return NotFound();
+					}
 				}
 				else
 				{
-					return NotFound();
+					return Ok(Data.Users.GetUsers());
 				}
 			}
 
 			return BadRequest("No parameters");
-		}
-
-		private IEnumerable<User> GetUsers(KeyValuePair<string, string> parameter)
-		{
-			List<User> result = new List<User>();
-			int id = -1;
-
-			Int32.TryParse(parameter.Value, out id);
-
-			if (id > 0)
-			{
-				if (_users.Any(u => u.Id == id))
-				{
-					result.AddRange(_users.Where(u => u.Id == id));
-				}
-			}
-			else
-			{
-				result.AddRange(_users);
-			}
-
-			return result;
 		}
 
 		private Dictionary<string, string> QueryStringToDictionaty(
---
+++ wpf/src/ConsumingWebApiFromWpf/SimpleHttpApi/DataLogic/Users.cs
@@ -0,0 +1,63 @@
+﻿namespace SimpleHttpApi.DataLogic
+{
+	using Models;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	public class Users
+	{
+		private static IEnumerable<User> _users = new List<User>()
+		{
+			new User(1, "User 1", new DateTime(2010, 1, 1), 1.23M, true),
+			new User(2, "User 2", new DateTime(2013, 12, 31), 2.34M, false),
+			new User(3, "User 3", new DateTime(2018, 3, 18), 4.56789M, true),
+		};
+
+		public static IEnumerable<User> GetUsers() => _users;
+
+		public static IEnumerable<User> GetUser(string parameter)
+		{
+			List<User> result = new List<User>();
+
+			var user = GetUserById(parameter);
+			if (user != null)
+			{
+				result.Add(user);
+			}
+
+			return result;
+		}
+
+		public static bool IsPowerUser(string parameter)
+		{
+			bool result = false;
+
+			var user = GetUserById(parameter);
+			if (user != null)
+			{
+				result = user.IsPower;
+			}
+
+			return result;
+		}
+
+		private static User GetUserById(string parameter)
+		{
+			User result = null;
+
+			int id = -1;
+			Int32.TryParse(parameter, out id);
+
+			if (id > 0)
+			{
+				if (_users.Any(u => u.Id == id))
+				{
+					result = _users.Where(u => u.Id == id).FirstOrDefault();
+				}
+			}
+
+			return result;
+		}
+	}
+}
