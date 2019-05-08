c9c47edda69817eb5176683214bf96eab83b1a51
Bojidar Ivanov <bojidarit@gmail.com>
2019-05-08 17:01:01 +0300

    Simple WPF Http client - Some models added, including base one.

---
+++ wpf/src/WPFClientForHttpApi/WPFSimpleHttpClient/Models/IdNameModel.cs
@@ -0,0 +1,32 @@
+﻿namespace WPFSimpleHttpClient.Models
+{
+	using Catel.Data;
+
+	public class IdNameModel : EntityBase
+	{
+		public IdNameModel() { }
+
+		public IdNameModel(int id, string name)
+		{
+			this.Id = id;
+			this.Name = name;
+		}
+
+		public int Id
+		{
+			get { return GetValue<int>(IdProperty); }
+			set { SetValue(IdProperty, value); }
+		}
+		public static readonly PropertyData IdProperty = RegisterProperty("Id", typeof(int), null);
+
+		public string Name
+		{
+			get { return GetValue<string>(NameProperty); }
+			set { SetValue(NameProperty, value); }
+		}
+		public static readonly PropertyData NameProperty = RegisterProperty("Name", typeof(string), null);
+
+		public override string ToString() =>
+			$"Id = {this.Id}; Name = '{this.Name}'";
+	}
+}
