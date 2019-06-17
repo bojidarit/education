67f8041e822aeb6fbdc712cecf25eb0452528313
Bojidar Ivanov <bojidarit@gmail.com>
2019-06-17 09:53:27 +0300

    Added to gitignore output folder used to store compiled staff. Added project that Identifies the version of the .NET Framework that a particular assembly was compiled against.

---
+++ misc/src/AppRuntimeVer/AppRuntimeVer/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace AppRuntimeVer
+{
+	using System.Linq;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			System.Reflection.Assembly assembly = null;
+
+			if (args != null && args.Length > 0)
+			{
+				assembly = System.Reflection.Assembly.LoadFrom(args[0]);
+
+				System.Console.WriteLine("{0} - Assembly Image Runtime Version",
+					assembly.ImageRuntimeVersion);
+				System.Console.WriteLine("{0} - Environment Version", System.Environment.Version);
+			}
+			else
+			{
+				assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+				System.Console.WriteLine("Hint: Put assembly name as application argument");
+				System.Console.WriteLine("{0} - Current Assembly Image Runtime Version", assembly.ImageRuntimeVersion);
+			}
+
+			System.Console.WriteLine("----------------------------------------");
+			System.Console.WriteLine("Help: Identifies the version of the .NET Framework that a particular assembly was compiled against.");
+			object[] list = assembly.GetCustomAttributes(true);
+			var attribute = list.OfType<System.Runtime.Versioning.TargetFrameworkAttribute>().FirstOrDefault();
+
+			if (attribute != null)
+			{
+				System.Console.WriteLine(attribute.FrameworkName);
+				System.Console.WriteLine(attribute.FrameworkDisplayName);
+			}
+
+			System.Console.ReadKey();
+		}
+	}
+}
