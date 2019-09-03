809ae056dbb1b0211e23b4e919db595470706fb5
Bojidar Ivanov <bojidarit@gmail.com>
2019-09-03 18:04:15 +0300

    React - JavaScript for React Developers from Mosh demo

--- react/mosh-crash-course/src/index.js
+++ react/mosh-crash-course/src/index.js
@@ -1,6 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// const element = <h1>Hello World...</h1>;
+// ReactDOM.render(element, document.getElementById("root"));
 
-const element = <h1>Hello World...</h1>;
-//console.log(element);
-ReactDOM.render(element, document.getElementById("root"));
+// JavaScript for React Developers | Mosh
+// https://youtu.be/NCwa_xi0Uuc
+
+// var -> function scope
+// let -> block scope
+function loopDemo() {
+  // for (var i = 0; i < 5; i++) {
+  for (let i = 0; i < 5; i++) {
+    console.log(`From loop -> ${i}`);
+  }
+
+  //console.log(`Outside loop -> ${i}`);
+}
+//loopDemo();
+
+// const -> constant (block scope)
+const PI = 3.14;
+console.log(PI);
+//PI = 3.1;
