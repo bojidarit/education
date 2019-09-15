55ea943090423d6a8b114f7b770226e43ee31263
Bojidar Ivanov <bojidarit@gmail.com>
2019-09-15 23:01:57 +0300

    react - JavaScript for React Developers demos from Mosh

--- react/mosh-crash-course/src/index.js
+++ react/mosh-crash-course/src/index.js
@@ -6,6 +6,10 @@
 // JavaScript for React Developers | Mosh
 // https://youtu.be/NCwa_xi0Uuc
 
+//-----------------------------------------------------------------------------
+// var, let and const
+//-----------------------------------------------------------------------------
+
 // var -> function scope
 // let -> block scope
 function loopDemo() {
@@ -16,9 +20,62 @@ function loopDemo() {
 
   //console.log(`Outside loop -> ${i}`);
 }
-//loopDemo();
+loopDemo();
 
 // const -> constant (block scope)
 const PI = 3.14;
 console.log(PI);
 //PI = 3.1;
+
+//-----------------------------------------------------------------------------
+// arrow functions
+//-----------------------------------------------------------------------------
+
+const pi = () => PI;
+console.log(`PI func returns ${pi()}`);
+
+{
+  const sqareFunc = number => number * number;
+  let number = 5;
+  console.log(`Sqare of ${number} equals ${sqareFunc(number)}`);
+
+  const persons = [
+    { id: 1, name: "first", isActive: true },
+    { id: 2, name: "second", isActive: false },
+    { id: 3, name: "third", isActive: true }
+  ];
+
+  console.log(persons);
+  console.log(persons.filter(item => item.isActive));
+}
+
+//-----------------------------------------------------------------------------
+// arrow functions and 'this'
+//-----------------------------------------------------------------------------
+
+const person = {
+  talk() {
+    console.log("talk this", this);
+  },
+  walk() {
+    var self = this;
+    setTimeout(function() {
+      console.log("walk self", self);
+    }, 100);
+  },
+  bless() {
+    setTimeout(() => console.log("bless arrow this", this), 1000);
+  }
+};
+
+person.talk();
+person.walk();
+person.bless();
+
+//-----------------------------------------------------------------------------
+// array.map()
+//-----------------------------------------------------------------------------
+
+const colors = ["red", "green", "blue"];
+const colorItems = colors.map(color => `<li>${color}</li>`);
+console.log(colorItems);
