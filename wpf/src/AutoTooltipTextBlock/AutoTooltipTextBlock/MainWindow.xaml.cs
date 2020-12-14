ee8921651ff981c3a00c8e32d6272af9f75a1cab
Bojidar Ivanov <bojidarit@gmail.com>
2020-12-14 18:45:44 +0200

    WPF - Automatically Showing ToolTips on a Trimmed TextBlock. An extra bonus there is Grid Utils for automatic RowDefinitions & ColumnDefinitions

---
+++ wpf/src/AutoTooltipTextBlock/AutoTooltipTextBlock/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace AutoTooltipTextBlock
+{
+	using System.Windows;
+	using System.Windows.Input;
+
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			KeyUp += this.MainWindow_KeyUp;
+		}
+
+		private void MainWindow_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Escape)
+			{
+				Application.Current.Shutdown(0);
+			}
+		}
+	}
+}
---
+++ wpf/src/AutoTooltipTextBlock/AutoTooltipTextBlock/Utils/TextBlockUtils.cs
@@ -0,0 +1,94 @@
+﻿namespace AutoTooltipTextBlock.Utils
+{
+	using System;
+	using System.Windows;
+	using System.Windows.Controls;
+
+	public class TextBlockUtils
+	{
+		#region AutoTooltip attached property
+
+		/// <summary>
+		/// Identified the attached AutoTooltip property. When true, this will set the TextBlock TextTrimming
+		/// property to WordEllipsis, and display a tooltip with the full text whenever the text is trimmed.
+		/// </summary>
+		public static readonly DependencyProperty AutoTooltipProperty = DependencyProperty.RegisterAttached(
+			"AutoTooltip",
+			typeof(bool),
+			typeof(TextBlockUtils),
+			new PropertyMetadata(false, OnAutoTooltipPropertyChanged));
+
+		/// <summary>
+		/// Gets the value of the AutoTooltipProperty dependency property
+		/// </summary>
+		public static bool GetAutoTooltip(DependencyObject obj)
+		{
+			return (bool)obj.GetValue(AutoTooltipProperty);
+		}
+
+		/// <summary>
+		/// Sets the value of the AutoTooltipProperty dependency property
+		/// </summary>
+		public static void SetAutoTooltip(DependencyObject obj, bool value)
+		{
+			obj.SetValue(AutoTooltipProperty, value);
+		}
+
+		#endregion
+
+
+		#region Logic
+
+		private static void OnAutoTooltipPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			TextBlock textBlock = d as TextBlock;
+			if (textBlock == null)
+			{
+				return;
+			}
+
+			if (e.NewValue.Equals(true))
+			{
+				textBlock.TextTrimming = TextTrimming.WordEllipsis;
+				ComputeAutoTooltip(textBlock);
+				textBlock.SizeChanged += TextBlock_SizeChanged;
+			}
+			else
+			{
+				textBlock.SizeChanged -= TextBlock_SizeChanged;
+			}
+		}
+
+		private static void TextBlock_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			TextBlock textBlock = sender as TextBlock;
+			if (textBlock == null)
+			{
+				return;
+			}
+
+			ComputeAutoTooltip(textBlock);
+		}
+
+		/// <summary>
+		/// Assigns the ToolTip for the given TextBlock based on whether the text is trimmed
+		/// </summary>
+		private static void ComputeAutoTooltip(TextBlock textBlock)
+		{
+			textBlock.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+			var width = textBlock.DesiredSize.Width;
+
+			if (textBlock.ActualWidth < width)
+			{
+				ToolTipService.SetToolTip(textBlock, textBlock.Text);
+			}
+			else
+			{
+				ToolTipService.SetToolTip(textBlock, null);
+			}
+
+		}
+
+		#endregion
+	}
+}
