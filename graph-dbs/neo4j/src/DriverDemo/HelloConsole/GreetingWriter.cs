cb0657c525140706d1c550b4b8c1e07ea479ae3c
Bojidar Ivanov <bojidarit@gmail.com>
2021-02-22 22:01:19 +0200

    Graph Databases - Neo4j driver demo.

---
+++ graph-dbs/neo4j/src/DriverDemo/HelloConsole/GreetingWriter.cs
@@ -0,0 +1,51 @@
+﻿namespace HelloConsole
+{
+	using Neo4j.Driver;
+	using System;
+	using System.Threading.Tasks;
+
+	public class GreetingWriter : IDisposable
+	{
+		private readonly IDriver driver;
+		private readonly string defaultUri = "bolt://localhost:7687";
+		private readonly string defaultUser = "neo4j";
+
+		public GreetingWriter(string password)
+		{
+			driver = GraphDatabase.Driver(defaultUri, AuthTokens.Basic(defaultUser, password));
+		}
+
+		public async ValueTask<string> WriteGreeting(string message)
+		{
+			if (driver == null)
+			{
+				return null;
+			}
+
+			IAsyncSession session = null;
+			try
+			{
+				session = driver.AsyncSession();
+				var record = await session.WriteTransactionAsync(tx =>
+				{
+					var result = tx.RunAsync("CREATE (a:Greeting) " +
+									"SET a.message = $message " +
+									"RETURN a.message + ', from node ' + id(a)",
+									new { message }).Result;
+					return result.SingleAsync();
+				}).ConfigureAwait(false);
+
+				return record[0].As<string>();
+			}
+			finally
+			{
+				await session?.CloseAsync();
+			}
+		}
+
+		public void Dispose()
+		{
+			driver?.Dispose();
+		}
+	}
+}
---
+++ graph-dbs/neo4j/src/DriverDemo/HelloConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace HelloConsole
+{
+	using System;
+	using System.IO;
+	using System.Threading.Tasks;
+
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			Console.Write("Enter password (:q to quit) -> ");
+			var pass = Console.ReadLine();
+
+			if (pass == ":q")
+			{
+				return;
+			}
+
+			try
+			{
+				var greetingWriter = new GreetingWriter(pass);
+				var result = await greetingWriter.WriteGreeting($"Hello {GetUniqueValue()}");
+				Console.WriteLine(result);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"ERROR {ex.GetType().FullName}: {ex.Message}");
+			}
+		}
+
+		static string GetUniqueValue()
+		{
+			var path = Path.GetRandomFileName();
+			return Path.GetFileName(path).Replace(".", string.Empty);
+		}
+	}
+}
