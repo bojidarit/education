5cf071341cee77f6673747d2e30d6b8861aef131
Bojidar Ivanov <bojidarit@gmail.com>
2021-02-23 10:28:32 +0200

    Neo4j Driver demo - adding empty .net 5 project manually.

---
+++ graph-dbs/neo4j/src/DriverDemo/MoviesConsole/MoviesProgram.cs
@@ -0,0 +1,16 @@
+﻿namespace MoviesConsole
+{
+	using System;
+	using System.Threading.Tasks;
+
+	class MoviesProgram
+	{
+		static async Task Main(string[] args)
+		{
+			Console.WriteLine(await DummyAsync());
+		}
+
+		static Task<string> DummyAsync() =>
+			Task.FromResult("Movies Demo....");
+	}
+}
