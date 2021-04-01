5431c13828404a941d07ee86f7f52a22272c4ead
Bojidar Ivanov <bojidarit@gmail.com>
2021-04-01 15:50:43 +0300

    Adding PDFsharp library with some demo projects directly from its GitHub samples repo.

---
+++ wpf/src/PDFsharpDemos/Graphics/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Graphics
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using PdfSharp.Fonts;
+    using PdfSharp.Pdf;
+
+    /// <summary>
+    /// This sample shows some of the capabilities of the XGraphcis class.
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            // Use WinAnsi (i.e. not Unicode) for all fonts if no XPdfFontOptions are specified.
+            GlobalFontSettings.DefaultFontEncoding = PdfFontEncoding.WinAnsi;
+
+            Document = new PdfDocument();
+            Document.Info.Title = "PDFsharp XGraphic Sample";
+            Document.Info.Author = "Stefan Lange";
+            Document.Info.Subject = "Created with code snippets that show the use of graphical functions";
+            Document.Info.Keywords = "PDFsharp, XGraphics";
+
+
+            // Create demonstration pages.
+            new LinesAndCurves().DrawPage(Document.AddPage());
+            new Shapes().DrawPage(Document.AddPage());
+            new Paths().DrawPage(Document.AddPage());
+            new Text().DrawPage(Document.AddPage());
+            new Images().DrawPage(Document.AddPage());
+
+            // Save the document...
+            const string filename = "HelloGraphics_tempfile.pdf";
+            var folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(folder, filename);
+
+            Console.WriteLine($"Will save '{path}'.{Environment.NewLine}Do you want to start a viewer (Y/n)?");
+            var keyInfo = Console.ReadKey();
+            var key = keyInfo.KeyChar.ToString().ToUpper();
+
+            Document.Save(path);
+
+            if (key == "N")
+            {
+                Console.Clear();
+                return;
+            }
+
+            // ...and start a viewer.
+            Process.Start(path);
+        }
+
+        internal static PdfDocument Document;
+    }
+}
+
---
+++ wpf/src/PDFsharpDemos/HelloWorld/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace HelloWorld
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using PdfSharp.Drawing;
+    using PdfSharp.Pdf;
+
+    /// <summary>
+    /// This sample is the obligatory Hello World program.
+    /// Source: https://github.com/empira/PDFsharp-samples
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            // Create a new PDF document.
+            var document = new PdfDocument();
+            document.Info.Title = "Created with PDFsharp";
+            document.Info.Author = "<Unknown Author>";
+            document.Info.CreationDate = DateTime.Today;
+            document.Info.Subject = "Demo purposes";
+
+            // Create an empty page in this document.
+            var page = document.AddPage();
+
+            // Get an XGraphics object for drawing on this page.
+            var gfx = XGraphics.FromPdfPage(page);
+
+            // Draw two lines with a red default pen.
+            var width = page.Width;
+            var height = page.Height;
+            gfx.DrawLine(XPens.Red, 0, 0, width, height);
+            gfx.DrawLine(XPens.Red, width, 0, 0, height);
+
+            // Draw a circle with a red pen which is 1.5 point thick.
+            var r = width / 5;
+            gfx.DrawEllipse(new XPen(XColors.Red, 1.5), XBrushes.White, new XRect(width / 2 - r, height / 2 - r, 2 * r, 2 * r));
+
+            // Create a font.
+            var font = new XFont("Times New Roman", 20, XFontStyle.BoldItalic);
+
+            // Draw the text.
+            gfx.DrawString("Hello, PDFsharp!", font, XBrushes.Black,
+                new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+
+            // Save the document...
+            var folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            const string filename = "HelloWorld_tempfile.pdf";
+            var path = Path.Combine(folder, filename);
+
+            Console.WriteLine($"Will save '{path}'.{Environment.NewLine}Do you want to start a viewer (Y/n)?");
+            var keyInfo = Console.ReadKey();
+            var key = keyInfo.KeyChar.ToString().ToUpper();
+
+            document.Save(path);
+
+            if (key == "N")
+            {
+                Console.Clear();
+                return;
+            }
+
+            // ...and start a viewer.
+            Process.Start(path);
+        }
+    }
+}
