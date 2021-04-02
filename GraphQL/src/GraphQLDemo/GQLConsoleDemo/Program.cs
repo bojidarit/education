ac3a7899f4fd5b240ca0ed4de9c4b9b3ef8e107c
Bojidar Ivanov <bojidarit@gmail.com>
2021-04-02 21:10:35 +0300

    GraphQL tests plus some assets for PDFsharp.

---
+++ GraphQL/src/GraphQLDemo/GQLConsoleDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace GQLConsoleDemo
+{
+	using System;
+	using System.Threading.Tasks;
+	using GraphQL;
+	using GraphQL.Types;
+
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var schema = Schema.For(@"
+			  type Query {
+				hello: String
+			  }
+			");
+
+			foreach (var item in schema.AllTypes)
+			{
+				Console.WriteLine(item);
+			}
+
+			//var json = await schema.ExecuteAsync(_ =>
+			//{
+			//	_.Query = "{ hello }";
+			//	_.Root = new { Hello = "Hello World!" };
+			//});
+
+			//Console.WriteLine(json);
+		}
+	}
+}
