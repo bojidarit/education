7d51299c3253b3ef8471cbe4e4e20a0d154ca16b
Bojidar Ivanov <bojidarit@gmail.com>
2021-06-23 16:21:57 +0300

    Bootstrap MVC 3.x Demo - Bootstrap's 4 Flexbox examples.

--- net-core/mvc-core-demo/src/BootstrapMVCDemo/MVCBootstrap43Core31/wwwroot/js/site.js
+++ net-core/mvc-core-demo/src/BootstrapMVCDemo/MVCBootstrap43Core31/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function openBootstrap4Tutorial() {
+    window.open('https://www.w3schools.com/bootstrap4/default.asp', '_blank').focus();
+};
+
+function openAllFontAwesomeIcons() {
+    window.open('https://fontawesome.com/v5.15/icons', '_blank').focus();
+};
