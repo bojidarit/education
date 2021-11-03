330161eecaf76f8548890d14ed5468001cd61259
Bojidar Ivanov <bojidarit@gmail.com>
2021-11-03 16:56:37 +0200

    Demo web apps for building Razor Pages dotnet core apps.

---
+++ net-core/mvc-core-demo/src/RazorPagesDemo/Net5Razor/Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿namespace Net5Razor.Pages
+{
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.AspNetCore.Mvc.RazorPages;
+
+	public class IndexModel : PageModel
+	{
+		[BindProperty]
+		public string Name { get; set; }
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
---
+++ net-core/mvc-core-demo/src/RazorPagesDemo/Net5Razor/Pages/Info.cshtml.cs
@@ -0,0 +1,11 @@
+namespace Net5Razor.Pages
+{
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class InfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
