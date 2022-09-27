e2f0b9d2ebc91421f68685a206deb9beb811a2b3
Bojidar Ivanov <bojidarit@gmail.com>
2022-09-27 17:43:48 +0300

    Pro ASP.NET Core 6 book - Chaper 4 clean web project with updgrades - not completed.

---
+++ net-core/book-pro-asp.net-core-6/src/ch-04/MySolution/MyProject/MyClass.cs
@@ -0,0 +1,8 @@
+// watch : Unable to apply hot reload because of a rude edit.
+// watch : Do you want to restart your app - Yes (y) / No (n) / Always (a) / Never (v)?
+
+namespace MyProject.MyNamespace;
+
+public class MyClass
+{
+}
