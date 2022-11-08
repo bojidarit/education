1e0b7a0d203a57d4a7f7ea0950f8c69c28b9124d
Bojidar Ivanov <bojidarit@gmail.com>
2022-11-08 22:26:54 +0200

    Micro-Python - Minor upgrade in the script for setting local time from NTP server and printing it formatted. Tested on ESP-32.

--- micro-python/networking/set_time_from_ntp.py
+++ micro-python/networking/set_time_from_ntp.py
@@ -6,6 +6,11 @@ import ntptime
 print("Setting time from NTP server...")
 ntptime.settime() # this queries the time from an NTP server
 
+
+def leadingZeroes(num, zeroes=2):
+    return ('{:0'+str(zeroes)+'d}').format(num)
+
+
 import time
 days = {0:"Monday", 1:"Tuesday", 2:"Wednesday", 3:"Thursday", 4:"Friday", 5:"Saturday", 6:"Sunday"}
 months = {1:"January", 2:"February", 3:"March", 4:"April", 5:"May", 6:"June", 7:"July", 8:"August", 9:"September", 10:"October", 11:"November", 12:"December"}
@@ -14,11 +19,8 @@ local_time = time.localtime(time.time() + UTC_OFFSET);
 (year, month, mday, hour, minute, second, weekday, yearday) = local_time
 print('-' * 77)
 # print("Local time in format (year, month, month-day, hour, min, second, weekday [Monday=0], year-day): ", local_time)
-print(str(mday) + "." + months[month] + "." + str(year) + " " + str(hour) + ":" + str(minute) + ":" + str(second), end=", ")
-print(days[weekday] + ", " + str(yearday) + " day of the year.")
+print(leadingZeroes(mday) + "." + months[month] + "." + leadingZeroes(year, 4), end=" ")
+print(leadingZeroes(hour) + ":" + leadingZeroes(minute) + ":" + leadingZeroes(second), end=", ")
+print(days[weekday] + ", " + leadingZeroes(yearday, 3) + " day of the year.")
 
-
-def leadingZeroes(num):
-    return '{:02d}'.format(num)
-
-print(leadingZeroes(local_time[3]) + ":" + leadingZeroes(local_time[4]))
+# print(leadingZeroes(local_time[3]) + ":" + leadingZeroes(local_time[4]))
