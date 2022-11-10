b8817dbf97c6ef0d642dd136b0a40abc46987e8a
Bojidar Ivanov <bojidarit@gmail.com>
2022-11-10 15:38:50 +0200

    Micro-Python - Updated demo for a clock on LCD display - better visualisation. Tested on a ESP-32 and ssd1306 driver.

--- micro-python/display/ssd1306/demo_lcd_clock.py
+++ micro-python/display/ssd1306/demo_lcd_clock.py
@@ -67,17 +67,44 @@ font_show_text(text, 3, 46, 16, False)
 
 days = {0:"Monday", 1:"Tuesday", 2:"Wednesday", 3:"Thursday", 4:"Friday", 5:"Saturday", 6:"Sunday"}
 # months = {1:"Jan", 2:"Feb", 3:"Mar", 4:"Apr", 5:"May", 6:"Jun", 7:"Jul", 8:"Aug", 9:"Sepr", 10:"Oct", 11:"Nov", 12:"Dec"}
-months = {1:"January", 2:"February", 3:"March", 4:"April", 5:"May", 6:"June", 7:"July", 8:"August", 9:"September", 10:"October", 11:"November", 12:"December"}
+months = {1:"January", 2:"February", 3:"March", 4:"April", 5:"May", 6:"June", 7:"July", 8:"August", 9:"Septembr", 10:"October", 11:"November", 12:"December"}
+
+
+def is_day_time(local_time):
+    return local_time[3] >= 7 and local_time[3] < 19
+
+# Contrast value must be between 0 and 255
+def set_contrast(local_time):
+    if is_day_time(local_time):
+        oled.contrast(255)
+    else:
+        oled.contrast(0)
 
 def show_time(local_time, colon):
     colon_txt = ":" if colon else " "
     time_str = leading_zeroes(local_time[3]) + colon_txt + leading_zeroes(local_time[4])
     font_show_text(time_str, 48, 36, 32, False)
+    set_contrast(local_time)
+
+def show_time_colon(flag):
+    colon_char = ":" if flag else " "
+    font_show_text(colon_char, 81, 36, 32, False)
+
+def say_ordinal(number):
+    num = number % 100 if number > 100 else number
+    num_str = str(num)[-1] if num < 10 or num > 19 else str(num)
+    if num_str == "1":
+        return "st"
+    if num_str == "2":
+        return "nd"
+    if num_str == "3":
+        return "rd"
+    return "th"
 
 def show_date(local_time, delimitter = "."):
-    oled.fill(0)
-    date_str = str(local_time[0]) + delimitter + months[local_time[1]] + delimitter + leading_zeroes(local_time[2])
-    font_show_text(days[local_time[6]], 1, 1, 16, False)
+    days_str = days[local_time[6]] + " " + leading_zeroes(local_time[7], 3) + say_ordinal(local_time[7])
+    font_show_text(days_str, 1, 1, 16, True)
+    date_str = str(local_time[2]) + delimitter + months[local_time[1]] + delimitter + leading_zeroes(local_time[0])
     font_show_text(date_str, 1, 18, 16, False)
 
 def date_changed(old_time, new_time):
@@ -89,16 +116,21 @@ def time_changed(old_time, new_time):
 
 oled.invert(False)
 oled.fill(0)
-print("Now showing time in a infinite loop...")
+print("Now showing time in an infinite loop...")
 
 utc_offset = 2 * 60 * 60   # East Europe Time Zone
-local_time = time.localtime(1)
+local_time = time.localtime(0)
+
+flag = True
 while True:
     old_time = local_time
-    local_time = time.localtime(time.time() + utc_offset);
+    local_time = time.localtime(time.time() + utc_offset)
     if date_changed(old_time, local_time):
         show_date(local_time)
+    daytime = is_day_time(local_time)
     if time_changed(old_time, local_time):
-        show_time(local_time, True)
+        show_time(local_time, not daytime)
+    if daytime:
+        show_time_colon(flag)
     time.sleep_ms(500)
-
+    flag = not flag
