9bd89adda41b22e9db5f7d6db1c1bd8d1cdec50d
Bojidar Ivanov <bojidarit@gmail.com>
2022-11-16 15:16:25 +0200

    Micropython - networking routines upgraded.

--- micro-python/networking/get_npm_datetime.py
+++ micro-python/networking/get_npm_datetime.py
@@ -1,13 +1,23 @@
 import manageWiFi as net
-print("Connected: ", net.connect())
-# net.disconnect(True)
+
+net.disconnect(True)
+net.connect_in_loop("mapex", "zdraveimapex", led_pin_no=15)
 
 import ntptime
 import time
 import localtimeUtils
 
-ntp_time = ntptime.time()
-# print(time.localtime(ntp_time))
+print("-" * 36)
+try:
+    # this queries the time from an NTP server
+    print("Querying NTP Server...")
+    ntptime.settime()
+except:
+    print("NTP's settime() rised an error, querying again...")
+    time.sleep(1)
+    ntptime.settime()
 
 print("Time from NTP server", end=": ")
+ntp_time = ntptime.time()
+# print(ntp_time)
 localtimeUtils.print_datetime(ntp_time, 2)
--- micro-python/networking/manageWiFi.py
+++ micro-python/networking/manageWiFi.py
@@ -1,5 +1,7 @@
-import network;
+import network
 import time
+import machine
+
 
 def decode_status(status_code):
     if status_code == network.STAT_IDLE:
@@ -19,7 +21,10 @@ def decode_status(status_code):
         
     return 'Status Code: ' + str(status_code)
 
-def connect(ssid, key, number_of_attempts = 15):
+# Led Pin: '2' for gemeral ESP 2, '15' for S2-Mini
+def connect(ssid, key, led_pin_no=2, number_of_attempts=10):
+    led = machine.Pin(led_pin_no, machine.Pin.OUT)
+    
     counter = 0;
 
     # STA_IF - Station Mode
@@ -27,6 +32,7 @@ def connect(ssid, key, number_of_attempts = 15):
 
     if wifi.active():
         if wifi.isconnected():
+            led.value(1)
             print("Already connected to Access-Point [" + wifi.config('essid') + "].")
             return True
     else:
@@ -37,23 +43,32 @@ def connect(ssid, key, number_of_attempts = 15):
 
     if not wifi.isconnected():
         print('Connetcting to Access-Point [' + ssid + ']...')
+        flag = True
         while (not wifi.isconnected() and counter < number_of_attempts):
             print(str(number_of_attempts - counter - 1), decode_status(wifi.status()))
             counter = counter + 1
             time.sleep(1)
+            led.value(1 if flag else 0)
+            flag = not flag
 
     print("-" * 36)
 
     if wifi.isconnected():
         print("Connected to Access-Point [" + ssid + "].")
         print("('IP', 'subnet mask', 'gateway', 'DNS'):", wifi.ifconfig())
-        print("RSSI:", wifi.status("rssi"), " with max transmit power ", wifi.config('txpower'), "dBm")
+        # print("RSSI:", wifi.status("rssi"), " with max transmit power ", wifi.config('txpower'), "dBm") # Do not wotk on S2-Mini
+        led.value(1)
         return True
     else:
         print("Still no connection after " + str(number_of_attempts) + " attempt(s)")
+        led.value(0)
         return False
 
-def disconnect(decativate = True):
+
+# Led Pin: '2' for gemeral ESP 2, '15' for S2-Mini
+def disconnect(led_pin_no=2, decativate=True):
+    led = machine.Pin(led_pin_no, machine.Pin.OUT)
+    led.value(0)
     wifi = network.WLAN(network.STA_IF)
     
     if (not wifi.active()):
@@ -71,3 +86,26 @@ def disconnect(decativate = True):
         wifi.active(False)
     else:
         print("... Wi-Fi is active: " + str(wifi.active()))
+
+
+# Led Pin: '2' for gemeral ESP 2, '15' for S2-Mini
+def connect_in_loop(ssid, key, led_pin_no=2, interations_count=-1, led_stay_on=False):
+    result = False
+    counter = 1
+    while (not result) or (interations_count > 0 and counter <= interations_count):
+        result = connect(ssid, key, led_pin_no)
+        print("-" * 36)
+        print("Connected:", result, "(Loop: " + str(counter) + ")")
+        counter += 1
+
+    if result:
+        time.sleep(1)
+        led = machine.Pin(led_pin_no, machine.Pin.OUT)
+        led.value(0)
+        flag = True
+        for i in range (0, 6):
+            time.sleep_ms(300)
+            led.value(flag)
+            flag = not flag
+
+    led.value(led_stay_on)
